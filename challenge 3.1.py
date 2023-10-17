a5af7cae0ef4dfbf8a378a1a16081af543b3e173
subithra2005 <148206361+subithra2005@users.noreply.github.com>
2023-10-17 15:08:08 +0530

    Add files via upload

---
+++ challenge 3.1.py
@@ -0,0 +1,25 @@
+# Python Program to depict multiple inheritance
+# when method is overridden in both classes
+
+# Search function with parameter list name 
+# and the value to be searched 
+
+
+def search(List, n): 
+
+  for i in range(len(List)): 
+    if List[i] == n: 
+      return True
+  return False
+
+
+# list which contains both string and numbers. 
+List = [1, 2, 'sachin', 4, 'Geeks', 6] 
+
+# Driver Code 
+n = 'Geeks'
+
+if search(List, n): 
+  print("Found") 
+else: 
+  print("Not Found") 
